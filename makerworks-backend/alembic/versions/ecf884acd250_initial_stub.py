1f96c6d71f1009f8d3d67d79c23db87bec79cc7a
schartrand77 <60296853+schartrand77@users.noreply.github.com>
2025-08-08 10:03:16 -0400

    Add initial Alembic stub migration ecf884acd250

---
+++ makerworks-backend/alembic/versions/ecf884acd250_initial_stub.py
@@ -0,0 +1,22 @@
+"""
+initial stub
+
+Revision ID: ecf884acd250
+Revises:
+Create Date: 2025-08-08 09:00:00
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = 'ecf884acd250'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    pass
+
+def downgrade():
+    pass

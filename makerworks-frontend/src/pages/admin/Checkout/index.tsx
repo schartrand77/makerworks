8faea50c4b059493a986f6ce17f9e0546756edc5
schartrand77 <60296853+schartrand77@users.noreply.github.com>
2025-08-20 19:30:20 -0400

    release

---
+++ makerworks-frontend/src/pages/admin/Checkout/index.tsx
@@ -0,0 +1,4 @@
+// src/pages/Checkout/index.tsx
+// shim: covers lazy(() => import('@/pages/Checkout')) and lazy(() => import('@/pages/Checkout/index'))
+export { default } from "../CheckoutPage";
+export * from "../CheckoutPage";
